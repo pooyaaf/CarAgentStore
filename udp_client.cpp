77ce93ff2a200aa40f4692d0817a3ef8bc45520e
pooyaaf <pooya1380@gmail.com>
2023-06-29 08:02:19 +0100

    Add UDP

--- udp_client.cpp
+++ udp_client.cpp
@@ -14,71 +14,63 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 #include <fstream>
+
 using namespace std;
+
+#define PORT 8080
+#define MAXLINE 1500
 // Client side
 int main(int argc, char *argv[])
 {
 
-    // we need 2 things: ip address and port number, in that order
-    if (argc != 3)
+    int sockfd;
+    char buffer[MAXLINE];
+    char msg[MAXLINE];
+    const char *hello = "Hello from client";
+    struct sockaddr_in servaddr;
+
+    // Creating socket file descriptor
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
     {
-        cerr << "Usage: ip_address port" << endl;
-        exit(0);
-    } // grab the IP address and port number
-    char *serverIp = argv[1];
-    int port = atoi(argv[2]);
-    // create a message buffer
-    char msg[1500];
-    // setup a socket and connection tools
-    struct hostent *host = gethostbyname(serverIp);
-    sockaddr_in sendSockAddr;
-    bzero((char *)&sendSockAddr, sizeof(sendSockAddr));
-    sendSockAddr.sin_family = AF_INET;
-    sendSockAddr.sin_addr.s_addr =
-        inet_addr(inet_ntoa(*(struct in_addr *)*host->h_addr_list));
-    sendSockAddr.sin_port = htons(port);
-    int clientSd = socket(AF_INET, SOCK_STREAM, 0);
-    // try to connect...
-    int status = connect(clientSd,
-                         (sockaddr *)&sendSockAddr, sizeof(sendSockAddr));
-    if (status < 0)
-    {
-        cout << "Error connecting to socket!" << endl;
-        return -1;
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
     }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+
+    // Filling server information
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(PORT);
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+
     cout << "Connected to the server!" << endl;
-    int bytesRead, bytesWritten = 0;
-    struct timeval start1, end1;
-    gettimeofday(&start1, NULL);
+
+    int n;
+    socklen_t len;
+    // send hello from client
+    sendto(sockfd, (const char *)hello, strlen(hello),
+           MSG_CONFIRM, (const struct sockaddr *)&servaddr,
+           sizeof(servaddr));
+    std::cout << "Hello message sent from client." << std::endl;
+
     while (1)
     {
+        n = recvfrom(sockfd, (char *)buffer, MAXLINE,
+                     MSG_WAITALL, (struct sockaddr *)&servaddr,
+                     &len);
+        buffer[n] = '\0';
+        std::cout << "Server :" << buffer << std::endl;
+
         cout << ">";
         string data;
         getline(cin, data);
         memset(&msg, 0, sizeof(msg)); // clear the buffer
         strcpy(msg, data.c_str());
-        if (data == "exit")
-        {
-            send(clientSd, (char *)&msg, strlen(msg), 0);
-            break;
-        }
-        bytesWritten += send(clientSd, (char *)&msg, strlen(msg), 0);
-        cout << "Awaiting server response..." << endl;
-        memset(&msg, 0, sizeof(msg)); // clear the buffer
-        bytesRead += recv(clientSd, (char *)&msg, sizeof(msg), 0);
-        if (!strcmp(msg, "exit"))
-        {
-            cout << "Server has quit the session" << endl;
-            break;
-        }
-        cout << "Server: " << msg << endl;
+        sendto(sockfd, (const char *)msg, strlen(msg),
+               MSG_CONFIRM, (const struct sockaddr *)&servaddr,
+               sizeof(servaddr));
     }
-    gettimeofday(&end1, NULL);
-    close(clientSd);
-    cout << "********Session********" << endl;
-    cout << "Bytes written: " << bytesWritten << " Bytes read: " << bytesRead << endl;
-    cout << "Elapsed time: " << (end1.tv_sec - start1.tv_sec)
-         << " secs" << endl;
-    cout << "Connection closed" << endl;
+
+    close(sockfd);
     return 0;
 }
--- udp_server.cpp
+++ udp_server.cpp
@@ -18,6 +18,7 @@
 #include <arpa/inet.h>
 
 #define PORT 8080
+#define MAXLINE 1500
 
 using namespace std;
 
@@ -37,48 +38,41 @@ int main(int argc, char *argv[])
     }
     // grab the port number
     int port = atoi(argv[1]);
+    int sockfd;
     // buffer to send and receive messages with
-    char msg[1500];
+    char buffer[MAXLINE];
+    char msg[MAXLINE];
+    const char *hello = "Hello from server";
+    struct sockaddr_in servaddr, cliaddr;
 
-    // setup a socket and connection tools
-    sockaddr_in servAddr;
-    bzero((char *)&servAddr, sizeof(servAddr));
-    servAddr.sin_family = AF_INET;
-    servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servAddr.sin_port = htons(port);
+    // Creating socket file descriptor
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
 
-    // open stream oriented socket with internet address
-    // also keep track of the socket descriptor
-    int serverSd = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverSd < 0)
+    memset(&servaddr, 0, sizeof(servaddr));
+    memset(&cliaddr, 0, sizeof(cliaddr));
+
+    // Filling server information
+    servaddr.sin_family = AF_INET; // IPv4
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(PORT);
+
+    // Bind the socket with the server address
+    if (bind(sockfd, (const struct sockaddr *)&servaddr,
+             sizeof(servaddr)) < 0)
     {
-        cerr << "Error establishing the server socket" << endl;
-        exit(0);
+        perror("bind failed");
+        exit(EXIT_FAILURE);
     }
-    // bind the socket to its local address
-    int bindStatus = bind(serverSd, (struct sockaddr *)&servAddr,
-                          sizeof(servAddr));
-    if (bindStatus < 0)
-    {
-        cerr << "Error binding socket to local address" << endl;
-        exit(0);
-    }
-    cout << "Waiting for a client to connect..." << endl;
-    // listen for up to 5 requests at a time
-    listen(serverSd, 5);
-    // receive a request from client using accept
-    // we need a new address to connect with the client
-    sockaddr_in newSockAddr;
-    socklen_t newSockAddrSize = sizeof(newSockAddr);
-    // accept, create a new socket descriptor to
-    // handle the new connection with client
-    int newSd = accept(serverSd, (sockaddr *)&newSockAddr, &newSockAddrSize);
-    if (newSd < 0)
-    {
-        cerr << "Error accepting request from client!" << endl;
-        exit(1);
-    }
-    cout << "Connected with client!" << endl;
+
+    socklen_t len;
+    int n;
+
+    len = sizeof(cliaddr); // len is value/result
+
     // lets keep track of the session time
     struct timeval start1, end1;
     gettimeofday(&start1, NULL);
@@ -87,6 +81,12 @@ int main(int argc, char *argv[])
 
     // receive a message from the client (listen)
     cout << "Awaiting client response..." << endl;
+    // recieve heloo msg from client
+    n = recvfrom(sockfd, (char *)buffer, MAXLINE,
+                 MSG_WAITALL, (struct sockaddr *)&cliaddr,
+                 &len);
+    buffer[n] = '\0';
+    printf("Client : %s\n", buffer);
 
     int userChoice = 0;
     int valread;
@@ -94,11 +94,6 @@ int main(int argc, char *argv[])
     memset(&msg, 0, sizeof(msg));
     while (userChoice != 2)
     {
-        memset(&msg, 0, sizeof(msg)); // clear the buffer
-        bytesRead += recv(newSd, (char *)&msg, sizeof(msg), 0);
-        cout << "Client: " << msg << endl;
-        int userChoice = stoi(msg);
-        // Send the user menu to the client
         string menu = "";
         menu += "Car Agency Program\n";
         menu += "1. Login\n";
@@ -109,53 +104,84 @@ int main(int argc, char *argv[])
             menu += "4. Show Wallet\n";
             menu += "5. Show Purchased Cars\n";
         }
-        send(newSd, menu.c_str(), strlen(menu.c_str()), 0);
+
+        sendto(sockfd, menu.c_str(), strlen(menu.c_str()),
+               MSG_CONFIRM, (const struct sockaddr *)&cliaddr,
+               len);
+        cout << "menu sent." << endl;
+        memset(&msg, 0, sizeof(msg)); // clear the buffer
+        bytesRead += recvfrom(sockfd, (char *)msg, MAXLINE,
+                              MSG_WAITALL, (struct sockaddr *)&cliaddr,
+                              &len);
+        buffer[n] = '\0';
+        printf("Client : %s\n", msg);
+        userChoice = stoi(msg);
         switch (userChoice)
         {
 
         case 1:
         {
-            // Receive the login credentials from the client
-            char username[1024] = {0};
-            char password[1024] = {0};
-            send(newSd, "Enter username: ", strlen("Enter username: "), 0);
-            valread = read(newSd, username, 1024);
-            send(newSd, "Enter password: ", strlen("Enter password: "), 0);
-            valread = read(newSd, password, 1024);
+            char username[MAXLINE] = {0};
+            char password[MAXLINE] = {0};
+            // recive username
+            sendto(sockfd, "Enter username: ", strlen("Enter username: "),
+                   MSG_CONFIRM, (const struct sockaddr *)&cliaddr,
+                   len);
+            memset(&msg, 0, sizeof(msg)); // clear the buffer
+            bytesRead += recvfrom(sockfd, (char *)msg, MAXLINE,
+                                  MSG_WAITALL, (struct sockaddr *)&cliaddr,
+                                  &len);
+            strcpy(username, msg);
+            memset(&msg, 0, sizeof(msg));
+            // recive password
+            sendto(sockfd, "Enter password: ", strlen("Enter password: "),
+                   MSG_CONFIRM, (const struct sockaddr *)&cliaddr,
+                   len);
+            memset(&msg, 0, sizeof(msg)); // clear the buffer
+            bytesRead += recvfrom(sockfd, (char *)msg, MAXLINE,
+                                  MSG_WAITALL, (struct sockaddr *)&cliaddr,
+                                  &len);
+            strcpy(password, msg);
+            memset(&msg, 0, sizeof(msg));
+
             if (agency.checkUserExists(username))
             {
                 User &user = agency.findUser(username);
                 if (user.checkPassword(password))
                 {
                     loggedInUser = &user;
-                    cout << "User:" << username << " Logged in with password:\n"
-                         << password;
-                    send(newSd, "Login successful\n", strlen("Login successful\n"), 0);
-                    memset(&msg, 0, sizeof(msg));
+                    cout << "User:" << username << " Logged in with password:" << password << "\n ";
+                    // sendto(sockfd, "Login successful ", strlen("Login successful"), MSG_CONFIRM, (const struct sockaddr *)&cliaddr, len);
+                    // memset(&msg, 0, sizeof(msg));
                     break;
                 }
                 else
                 {
-                    send(newSd, "Incorrect password\n", strlen("Incorrect password\n"), 0);
+                    sendto(sockfd, "Incorrect password", strlen("Incorrect password"),
+                           MSG_CONFIRM, (const struct sockaddr *)&cliaddr,
+                           len);
                     memset(&msg, 0, sizeof(msg));
                     break;
                 }
             }
             else
             {
-                send(newSd, "User does not exist\n", strlen("User does not exist\n"), 0);
+                sendto(sockfd, "User does not exist", strlen("User does not exist"),
+                       MSG_CONFIRM, (const struct sockaddr *)&cliaddr,
+                       len);
                 memset(&msg, 0, sizeof(msg));
                 break;
             }
         }
         case 2:
         {
-            send(newSd, "Exiting program\n", strlen("Exiting program\n"), 0);
+            sendto(sockfd, "Exiting program", strlen("Exiting program"),
+                   MSG_CONFIRM, (const struct sockaddr *)&cliaddr,
+                   len);
             memset(&msg, 0, sizeof(msg));
             // we need to close the socket descriptors after we're all done
             gettimeofday(&end1, NULL);
-            close(newSd);
-            close(serverSd);
+            close(sockfd);
             cout << "********Session********" << endl;
             cout << "Bytes written: " << bytesWritten << " Bytes read: " << bytesRead << endl;
             cout << "Elapsed time: " << (end1.tv_sec - start1.tv_sec)
@@ -167,63 +193,121 @@ int main(int argc, char *argv[])
         {
             if (loggedInUser)
             {
-
-                send(newSd, "Available cars: (Enter to list view | cntrl + c to exit)", strlen("Available cars: (Enter to list view | cntrl + c to exit)"), 0);
+                sendto(sockfd, "Available cars: (Enter to list view | cntrl + c to exit)", strlen("Available cars: (Enter to list view | cntrl + c to exit)"),
+                       MSG_CONFIRM, (const struct sockaddr *)&cliaddr,
+                       len);
+                bytesRead += recvfrom(sockfd, (char *)msg, MAXLINE,
+                                      MSG_WAITALL, (struct sockaddr *)&cliaddr,
+                                      &len);
+                memset(&msg, 0, sizeof(msg));
                 std::string availableCars = agency.showCars();
-                send(newSd, availableCars.c_str(), availableCars.size(), 0);
-                send(newSd, "Enter the model of the car you want to buy: ", strlen("Enter the model of the car you want to buy: "), 0);
-                // Receive the user's choice from the client for model
+                availableCars = availableCars + "\nEnter the model of the car you want to buy: ";
+                sendto(sockfd, availableCars.c_str(), availableCars.size(),
+                       MSG_CONFIRM, (const struct sockaddr *)&cliaddr,
+                       len);
+
                 char model[1024] = {0};
-                // memset(&msg, 0, sizeof(msg));
-                // bytesRead += recv(newSd, (char *)&model, sizeof(model), 0);
-                valread = read(newSd, model, 1024);
-                cout << model;
-                // cout<<"Selected car model :"<<model;
+                bytesRead += recvfrom(sockfd, (char *)model, MAXLINE,
+                                      MSG_WAITALL, (struct sockaddr *)&cliaddr,
+                                      &len);
                 if (agency.checkCarExists(model))
                 {
                     Car &car = agency.findCar(model);
+
                     if (loggedInUser->getWallet() >= car.getPrice())
                     {
                         if (agency.buyCar(*loggedInUser, model, pathOfUsersJSON, pathOfCarsJSON))
                         {
-                            send(newSd, "Car purchased successfully", strlen("Car purchased successfully"), 0);
+
+                            sendto(sockfd, "Car purchased successfully( Enter to back to menu | cntrl + c to exit)", strlen("Car purchased successfully( Enter to back to menu | cntrl + c to exit)"),
+                                   MSG_CONFIRM, (const struct sockaddr *)&cliaddr,
+                                   len);
+                            bytesRead += recvfrom(sockfd, (char *)msg, MAXLINE,
+                                                  MSG_WAITALL, (struct sockaddr *)&cliaddr,
+                                                  &len);
+                            memset(&msg, 0, sizeof(msg));
                         }
                         else
                         {
-                            send(newSd, "Error purchasing car", strlen("Error purchasing car"), 0);
+
+                            sendto(sockfd, "Error purchasing car( Enter to back to menu | cntrl + c to exit)", strlen("Error purchasing car( Enter to back to menu | cntrl + c to exit)"),
+                                   MSG_CONFIRM, (const struct sockaddr *)&cliaddr,
+                                   len);
+                            bytesRead += recvfrom(sockfd, (char *)msg, MAXLINE,
+                                                  MSG_WAITALL, (struct sockaddr *)&cliaddr,
+                                                  &len);
+                            memset(&msg, 0, sizeof(msg));
                         }
                     }
                     else
                     {
-                        send(newSd, "You do not have enough money", strlen("You do not have enough money"), 0);
+                        sendto(sockfd, "You do not have enough money( Enter to back to menu | cntrl + c to exit)", strlen("You do not have enough money( Enter to back to menu | cntrl + c to exit)"),
+                               MSG_CONFIRM, (const struct sockaddr *)&cliaddr,
+                               len);
+                        bytesRead += recvfrom(sockfd, (char *)msg, MAXLINE,
+                                              MSG_WAITALL, (struct sockaddr *)&cliaddr,
+                                              &len);
+                        memset(&msg, 0, sizeof(msg));
                     }
                 }
                 else
                 {
-                    send(newSd, "Car does not exist", strlen("Car does not exist"), 0);
+                    sendto(sockfd, "Car does not exist( Enter to back to menu | cntrl + c to exit)", strlen("Car does not exist( Enter to back to menu | cntrl + c to exit)"),
+                           MSG_CONFIRM, (const struct sockaddr *)&cliaddr,
+                           len);
+                    bytesRead += recvfrom(sockfd, (char *)msg, MAXLINE,
+                                          MSG_WAITALL, (struct sockaddr *)&cliaddr,
+                                          &len);
+                    memset(&msg, 0, sizeof(msg));
                 }
             }
             else
             {
-                send(newSd, "You must log in first", strlen("You must log in first"), 0);
+                sendto(sockfd, "You must log in first( Enter to back to menu | cntrl + c to exit)", strlen("You must log in first( Enter to back to menu | cntrl + c to exit)"),
+                       MSG_CONFIRM, (const struct sockaddr *)&cliaddr,
+                       len);
+                bytesRead += recvfrom(sockfd, (char *)msg, MAXLINE,
+                                      MSG_WAITALL, (struct sockaddr *)&cliaddr,
+                                      &len);
+                memset(&msg, 0, sizeof(msg));
             }
             break;
         }
-
         case 4:
         {
-            string walletMsg = "Your wallet balance is: " + std::to_string(loggedInUser->getWallet());
-            send(newSd, walletMsg.c_str(), walletMsg.length(), 0);
+            string walletMsg = "Your wallet balance is: " + std::to_string(loggedInUser->getWallet()) + "\n( Enter to back to menu | cntrl + c to exit)";
+            sendto(sockfd, walletMsg.c_str(), walletMsg.length(),
+                   MSG_CONFIRM, (const struct sockaddr *)&cliaddr,
+                   len);
+            bytesRead += recvfrom(sockfd, (char *)msg, MAXLINE,
+                                  MSG_WAITALL, (struct sockaddr *)&cliaddr,
+                                  &len);
+            memset(&msg, 0, sizeof(msg));
             break;
         }
         case 5:
         {
-            std::string purchasedCarsStr = loggedInUser->showPurchasedCars();
-            send(newSd, purchasedCarsStr.c_str(), purchasedCarsStr.length(), 0);
+            std::string purchasedCarsStr = loggedInUser->showPurchasedCars() + "\n( Enter to back to menu | cntrl + c to exit)";
+            sendto(sockfd, purchasedCarsStr.c_str(), purchasedCarsStr.length(),
+                   MSG_CONFIRM, (const struct sockaddr *)&cliaddr,
+                   len);
+            bytesRead += recvfrom(sockfd, (char *)msg, MAXLINE,
+                                  MSG_WAITALL, (struct sockaddr *)&cliaddr,
+                                  &len);
+            memset(&msg, 0, sizeof(msg));
             break;
         }
         }
     }
+    // n = recvfrom(sockfd, (char *)buffer, MAXLINE,
+    //              MSG_WAITALL, (struct sockaddr *)&cliaddr,
+    //              &len);
+    // buffer[n] = '\0';
+    // printf("Client : %s\n", buffer);
+    // sendto(sockfd, (const char *)hello, strlen(hello),
+    //        MSG_CONFIRM, (const struct sockaddr *)&cliaddr,
+    //        len);
+    // std::cout << "Hello message sent." << std::endl;
 
     return 0;
 }
